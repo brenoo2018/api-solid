66623f689c31e5490be49efa66aa3b2d5970ed3a
Taynan Breno <taynan.breno@oqvocequer.com.br>
2023-04-26 12:24:50 -0300

    creating password hashes

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
 import { prisma } from '@/lib/prisma';
+import { hash } from 'bcryptjs';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -11,8 +12,20 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
+  const findUser = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (findUser) {
+    return reply.status(409).send();
+  }
+
+  const password_hash = await hash(password, 6);
+
   await prisma.user.create({
-    data: { name, email, password_hash: password },
+    data: { name, email, password_hash },
   });
 
   return reply.status(201).send();
