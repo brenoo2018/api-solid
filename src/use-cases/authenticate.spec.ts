9f230b00adad6174a6516f62e5ac3a3eecf28f55
Taynan Breno <taynan.breno@oqvocequer.com.br>
2023-04-26 23:51:21 -0300

    creating use case for the profile

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -1,8 +1,8 @@
 import { expect, test, describe, it, beforeEach } from 'vitest';
 import { compare, hash } from 'bcryptjs';
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository';
-import { AuthenticateUseCase } from '../authenticate';
-import { InvalidCredentialsError } from './invalid-credentials-error';
+import { AuthenticateUseCase } from './authenticate';
+import { InvalidCredentialsError } from './errors/invalid-credentials-error';
 
 let usersRepository: InMemoryUsersRepository;
 let sut: AuthenticateUseCase;
