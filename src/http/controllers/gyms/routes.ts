6327f3fc55f8bf58e04c52557d77e91c7f2fb597
Taynan Breno <taynan.breno@oqvocequer.com.br>
2023-06-08 14:55:16 -0300

    Gym search and nearby controller

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,7 +1,14 @@
 import { FastifyInstance } from 'fastify';
 
 import { verifyJwt } from '@/http/middlewares/verify-jwt';
+import { search } from './search';
+import { nearby } from './nearby';
+import { register } from './register';
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt);
+
+  app.get('/gyms/search', search);
+  app.get('/gyms/nearby', nearby);
+  app.post('/gyms/create', register);
 }
